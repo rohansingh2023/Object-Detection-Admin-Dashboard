3c17c5b74987987ae2881cfd18375e1c5b8f4852
Rohan Singh <65129632+rohansingh2023@users.noreply.github.com>
2022-05-24 23:25:28 +0530

    make it better

--- src/requestMethod.js
+++ src/requestMethod.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const BASE_URL = "https://adminobj.herokuapp.com/api";
+const BASE_URL2 = "https://objadminapi.herokuapp.com/api";
 
 export const publicRequest = axios.create({
-  baseURL: BASE_URL,
+  baseURL: BASE_URL2,
 });
